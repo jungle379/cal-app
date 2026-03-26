2db0016106f5869ecb1f63ec09760a1151e4616a
jungle379 <jungle379@gmail.com>
2026-03-26 10:10:33 +0900

    トークン追加

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -3,4 +3,10 @@ import { createClient } from "@supabase/supabase-js";
 export const supabase = createClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL!,
   process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_DEFAULT_KEY!,
+  {
+    auth: {
+      persistSession: true,
+      autoRefreshToken: true,
+    },
+  },
 );
