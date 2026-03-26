c6a6648945a96cfc8b3c87e0eb78121309eb6af4
jungle379 <jungle379@gmail.com>
2026-03-26 12:53:27 +0900

    button化

--- app/page.tsx
+++ app/page.tsx
@@ -5,12 +5,15 @@ import { useRouter } from "next/navigation";
 export default function HomePage() {
   const router = useRouter();
   // 未ログインならログインページへ
-  router.push("/login");
+  const handleLogin = () => {
+    router.push("/login");
+  };
 
   // 即リダイレクトなので Loading 表示は最小限
   return (
     <div className="min-h-screen flex items-center justify-center">
       <p className="text-gray-500">Loading...</p>
+      <button onClick={handleLogin}>button</button>
     </div>
   );
 }
