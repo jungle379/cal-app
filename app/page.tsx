266ef7ab2eb371a438b6990adcb7d4a2c8af3e54
hirokimorino <jungle379@gmail.com>
2026-05-22 16:02:52 +0900

    トップにアイコン追加

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { Button, Container, Title, Center, Box } from "@mantine/core";
+import { Button, Container, Title, Center, Box,Image } from "@mantine/core";
 import { useRouter } from "next/navigation";
 
 export default function HomePage() {
@@ -13,6 +13,16 @@ export default function HomePage() {
         <Title order={1} style={{ textAlign: "center" }}>
           共有カレンダー
         </Title>
+      </Container>
+
+            <Container size="sm" py="xl" style={{ display: "flex", justifyContent: "center", height: "200px",width: "200px"  }}>
+          <Image
+      radius="md"
+      height="auto"
+      fit="contain"
+      width="auto"
+      src="/icon-192.png"
+    />
       </Container>
 
       {/* 画面の高さ: 半分より少し下（例: 55%） */}
